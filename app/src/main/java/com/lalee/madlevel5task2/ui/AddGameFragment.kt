2200cd6725bec1248210c47a79dd378ab2a7558d
Sagi Lalee <Sagi_lalee@hotmail.com>
2020-10-20 22:05:31 +0200

    added the layout for the fragments

--- app/src/main/java/com/lalee/madlevel5task2/ui/AddGameFragment.kt
+++ app/src/main/java/com/lalee/madlevel5task2/ui/AddGameFragment.kt
@@ -1,22 +1,23 @@
-package com.lalee.madlevel5task2
+package com.lalee.madlevel5task2.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.lalee.madlevel5task2.R
 
 /**
- * A simple [Fragment] subclass as the default destination in the navigation.
+ * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class GameOverviewFragment : Fragment() {
+class AddGameFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game_overview, container, false)
+        return inflater.inflate(R.layout.fragment_add_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
